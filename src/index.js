8462d4b8a14b1f62ffd0cb567ffdabbc46c6d6dc
Idris <idrisaboladeo@gmail.com>
2020-02-05 12:46:40 +0100

    add route to update user

--- src/index.js
+++ src/index.js
@@ -12,23 +12,23 @@ app.use(express.json());
 
 app.post('/user', async (req, res) => {
   console.log(req.body)
-  if(!req.body) return;
+  if (!req.body) return;
   const user = await new User(req.body);
   try {
     await user.save()
     return res.status(201).send(user)
-  } catch (error) { 
+  } catch (error) {
     res.status(400).send(e)
   }
 
   // user.save().then((user) => res.status(201).send(user)).catch(e => res.status(400).send(e))
-}) 
+})
 
 app.get('/users', async (req, res) => {
   const user = await User.find({})
   try {
     res.status(200).send(user)
-    
+
   } catch (error) {
     res.status(401).send(error)
   }
@@ -37,28 +37,51 @@ app.get('/users', async (req, res) => {
 app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
   try {
-       const user = await User.findById(_id)
-        res.status(200).send(user)
+    const user = await User.findById(_id)
+    res.status(200).send(user)
   } catch (error) {
     res.status(404).send(error)
   }
 })
 
-app.post('/task', async (req, res) => {
-  console.log(req.body)
-  if(!req.body) return;
-  const task =  new Tasks(req.body);
+app.patch('/users/:id', async (req, res) => {
+  console.log(Object.keys(req.body))
+  const updates = Object.keys(req.body)
+  const valuesToUpdate = ["name", "email", "age", "password"]
+  const isValidUpdate = updates.every(update => valuesToUpdate.includes(update))
+
+  if (!isValidUpdate) {
+    res.status(404).send({ error: " invalid update" })
+  }
+
   try {
-    await task.save();
-    res.status(201).send(task)
-    
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    if (!user) {
+      return res.status(404).send({ error: "user doesn't exist on the database" })
+    }
+
+    return res.status(200).send(user)
   } catch (error) {
     res.status(400).send(error)
   }
-}) 
+})
+
+app.post('/task', async (req, res) => {
+  console.log(req.body)
+  if (!req.body) return;
+  const task = new Tasks(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task)
+
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
 
 app.get('/task', async (req, res) => {
-  
+
   try {
     const task = await Tasks.find({})
     res.status(200).send(task)
